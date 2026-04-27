196bc38ed0082982627d3d9b475d80ec22fb406c
Emin Zeynalov <emin.proger@gmail.com>
2026-04-27 12:49:56 +0400

    test

--- android/app/src/main/java/com/sati/svveetideas/MainApplication.kt
+++ android/app/src/main/java/com/sati/svveetideas/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sweetideas
+package com.sati.svveetideas
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/SatiSvveetIdeas/AppDelegate.swift
+++ ios/SatiSvveetIdeas/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "SweetIdeas",
+      withModuleName: "Sati: Svveet Ideas",
       in: window,
       launchOptions: launchOptions
     )
